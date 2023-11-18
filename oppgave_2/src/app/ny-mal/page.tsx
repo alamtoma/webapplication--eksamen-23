2c718cf9911acf75b305c6a0d9efc6e083e6533f
Didrik Sandmark <111964538+timdidrik@users.noreply.github.com>
2023-11-18 03:14:58 +0100

    Added a page for NyMal.

---
+++ oppgave_2/src/app/ny-mal/page.tsx
@@ -0,0 +1,10 @@
+import NavigationBar from "@/components/NavigationBar";
+
+export default function NyMal() {
+    return (
+        <>
+        <NavigationBar />
+        <p>Gods of death</p>
+        </>
+    )
+}
