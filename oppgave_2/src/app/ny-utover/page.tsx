fe3ba191304d7ac2d846b53301cd957b44e6199d
Didrik Sandmark <111964538+timdidrik@users.noreply.github.com>
2023-11-18 03:14:39 +0100

    Added a page for NyUtover.

---
+++ oppgave_2/src/app/ny-utover/page.tsx
@@ -0,0 +1,10 @@
+import NavigationBar from "@/components/NavigationBar"
+
+export default function NyUtover() {
+    return (
+        <>
+        <NavigationBar />
+        <p>Do you know?</p>
+        </>
+    )
+}
