832a0759c5a855f1f97de3696ffc8d4e74d2fea2
Didrik Sandmark <111964538+timdidrik@users.noreply.github.com>
2023-11-21 02:02:39 +0100

    Removed unnecessary div element for styling.

--- oppgave_2/src/components/PageButton.tsx
+++ oppgave_2/src/components/PageButton.tsx
@@ -1,10 +1,6 @@
-
-
 // Need types for typescript later
 export default function PageButton({onClick, label}) {
     return (
-        <div className="center-container">
-                <button className="page-button" onClick={onClick}>{label}</button>
-        </div>
+        <button className="page-button" onClick={onClick}>{label}</button>
     )
 }
