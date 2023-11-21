0dc1a8ffa83b9ae6a017a44554379ad70ede4bbf
Didrik Sandmark <111964538+timdidrik@users.noreply.github.com>
2023-11-21 03:26:07 +0100

    Added an unfinished table that will show athletes.

--- oppgave_2/src/app/page.tsx
+++ oppgave_2/src/app/page.tsx
@@ -1,10 +1,48 @@
+'use client'
+
 import NavigationBar from "@/components/NavigationBar"
+import PageButton from "@/components/PageButton"
 
 export default function Home() {
+
+  const handleAddWorkoutButton = () => {
+    console.log("This takes you somewhere to make a workout.")
+  }
+
   return (
     <>
     <NavigationBar />
-    <p>Hello there.</p>
+    <PageButton onClick={handleAddWorkoutButton} label="New workout"/>
+
+    <div className="heading">
+      <h1>Current athletes:</h1>
+    </div>
+    
+    {/* Should be something like this, I imagine, for the table */}
+    {/* src: https://chat.openai.com 
+    
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Athlete ID</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {athletes.map((athlete) => (
+            <tr key={athlete.id}>
+              <td>
+                <Link href={`/athlete/${athlete.id}`}>
+                  <a>{athlete.id}</a>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    */}
     </>
   )
 }
