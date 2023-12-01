97d3c179e6e6e38a2a551aadf77d5029e882877d
Didrik Sandmark <111964538+timdidrik@users.noreply.github.com>
2023-12-01 22:16:53 +0100

    Added a list of all questions.

--- oppgave_2/src/app/questions/page.tsx
+++ oppgave_2/src/app/questions/page.tsx
@@ -3,11 +3,13 @@
 import NavigationBar from "@/components/NavigationBar"
 import PageButton from "@/components/PageButton"
 import QuestionForm from "@/components/QuestionForm"
+import type { Question } from "@/features/types"
 import { useState } from "react"
 
 export default function Questions() {
 
     const [isQuestionFormVisible, setIsQuestionFormVisible] = useState(false)
+    const [questions, setQuestions] = useState<Question[]>([])
 
     const handleAddQuestionButton = () => {
         setIsQuestionFormVisible(true)
@@ -36,10 +38,24 @@ export default function Questions() {
 
         {isQuestionFormVisible && (
             <div className="center-container">
-                <QuestionForm onClose={handleCloseQuestionForm}/>
+                <QuestionForm onClose={handleCloseQuestionForm} setQuestions={setQuestions} questions={questions}/>
             </div>
         )}
-    
+
+        <div className="center-container">
+            <h2>List of all current questions:</h2>
+        </div>
+
+        <div className="center-container">
+            <ul>
+                {questions.map((question, index) => (
+                    <li key={index}>
+                        <p>{question.questionText}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+
         </>
     )
 }
