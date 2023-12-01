1a12dfba6fbf4e7cffeb9715e27fb1d8b4352a9f
Didrik Sandmark <111964538+timdidrik@users.noreply.github.com>
2023-12-01 18:18:26 +0100

    Added types for fake athlete data and questions.

---
+++ oppgave_2/src/features/types.ts
@@ -0,0 +1,31 @@
+// Semi-scuffed -> to test athlete rq
+export type Faker = {
+    id: () => string;
+}
+
+export type AthleteProps = {
+    id: string;
+}
+
+// Types etc. for questions
+// src: https://chat.openai.com
+export enum QuestionType {
+    TEXT = "Plain text",
+    RADIO_NUMBER = "Scale of 1-10",
+    RADIO_EMOJI = "Emojis: good, neutral or bad",
+}
+
+export const AnswerTypes: QuestionType[] = [
+    QuestionType.TEXT,
+    QuestionType.RADIO_NUMBER,
+    QuestionType.RADIO_EMOJI,
+]
+
+export type Question = {
+    questionText: string;
+    answerType: QuestionType;
+}
+
+export type QuestionFormProps = {
+    onClose: () => void;
+}
