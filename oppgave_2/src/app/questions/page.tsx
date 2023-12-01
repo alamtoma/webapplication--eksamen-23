c2b445dd899928b13ca06da06ed198c0ba7684ae
Didrik Sandmark <111964538+timdidrik@users.noreply.github.com>
2023-12-01 18:24:47 +0100

    Will now see a form after clicking add question button.

--- oppgave_2/src/app/questions/page.tsx
+++ oppgave_2/src/app/questions/page.tsx
@@ -1,21 +1,31 @@
 'use client'
 
-import NavigationBar from "@/components/NavigationBar";
-import PageButton from "@/components/PageButton";
+import NavigationBar from "@/components/NavigationBar"
+import PageButton from "@/components/PageButton"
+import QuestionForm from "@/components/QuestionForm"
+import { useState } from "react"
 
 export default function Questions() {
 
+    const [isQuestionFormVisible, setIsQuestionFormVisible] = useState(false)
+
     const handleAddQuestionButton = () => {
-        console.log("Logic for adding a question.")
+        setIsQuestionFormVisible(true)
+    }
+    
+    const handleCloseQuestionForm = () => {
+        setIsQuestionFormVisible(false)
     }
 
     // This function may not be needed!!
-    // May not need to have a remove button
+    // This function may not be needed!!
     const handleRemoveQuestionButton = () => {
         console.log("Logic for removing a question.")
     }
 
     return (
+
+        // Will fix button/text position
         <>
         <NavigationBar />
 
@@ -23,8 +33,13 @@ export default function Questions() {
             <PageButton onClick={handleAddQuestionButton} label="Add a new Question"/>
             <PageButton onClick={handleRemoveQuestionButton} label="Remove Question"/>
         </div>
-        
-        <p>Love apples.</p>
+
+        {isQuestionFormVisible && (
+            <div className="center-container">
+                <QuestionForm onClose={handleCloseQuestionForm}/>
+            </div>
+        )}
+    
         </>
     )
 }
